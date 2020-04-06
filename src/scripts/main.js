0ac6aca4693b604cee8b297fa30cc5f3ac72e19b
SDen4 <asw325@yandex.ru>
2020-04-06 20:54:04 +0500

    refactoring js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,6 @@ const wrapper = document.querySelector('.wrapper'),
       currentWinWidth = parseInt(window.getComputedStyle(wrapper).width),
       totalScrollHeight = parseInt(document.body.scrollHeight - document.documentElement.clientHeight);
 
-let currentY;
-
 window.addEventListener('scroll', () => {
-    currentY = window.pageYOffset;
-    indicator.style.width = (currentY/totalScrollHeight*currentWinWidth) + 'px';
+    indicator.style.width = (window.pageYOffset)/totalScrollHeight*currentWinWidth + 'px';
 });
